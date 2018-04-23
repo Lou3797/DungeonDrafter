c1c5036cc2c0ec363851dfa1c9682164d5d244c2
Nicholas Roth <lou3797@gmail.com>
2018-04-23 09:21:06 -0500

    Moved comparePoints out of BrushTool and into its parent class

--- src/drawtool/BrushTool.java
+++ src/drawtool/BrushTool.java
@@ -9,10 +9,6 @@ import javafx.scene.paint.Color;
 import map.Map;
 
 public class BrushTool extends PenTool {
-    private Rectangle2D viewPort;
-    private int brushSize = 10;
-    private Point2D min;
-    private Point2D max;
 
     @Override
     public void mousePressed(MouseEvent event, Map map) {
@@ -41,32 +37,6 @@ public class BrushTool extends PenTool {
     private void drawCircle(MouseEvent event, GraphicsContext gc) {
         gc.setFill(Color.GREEN);
         gc.fillOval(event.getX()-this.brushSize, event.getY()-this.brushSize, this.brushSize*2, this.brushSize*2);
-    }
-
-    private void comparePoints(MouseEvent event, Map map) {
-        if(event.getX()-this.brushSize < min.getX()) {
-            min = new Point2D(event.getX()-this.brushSize, min.getY());
-        } else if(event.getX()+this.brushSize > max.getX()) {
-            max = new Point2D(event.getX()+this.brushSize, max.getY());
-        }
-
-        if(event.getY()-this.brushSize < min.getY()) {
-            min = new Point2D(min.getX(), event.getY()-this.brushSize);
-        } else if (event.getY()+this.brushSize > max.getY()) {
-            max = new Point2D(max.getX(), event.getY()+this.brushSize);
-        }
-
-        if(event.getX() < 0) {
-            min = new Point2D(0, min.getY());
-        } else if (event.getX() > map.getWidth()) {
-            max = new Point2D(map.getWidth(), max.getY());
-        }
-
-        if(event.getY() < 0) {
-            min = new Point2D(min.getX(), 0);
-        } else if(event.getY() > map.getHeight()) {
-            max = new Point2D(max.getX(), map.getHeight());
-        }
     }
 
     private String getEventCoordinates(MouseEvent event) {
--- src/drawtool/PenTool.java
+++ src/drawtool/PenTool.java
@@ -1,9 +1,14 @@
 package drawtool;
 
+import javafx.geometry.Point2D;
 import javafx.scene.input.MouseEvent;
 import map.Map;
 
 public abstract class PenTool implements DrawStrategy {
+    protected int brushSize = 10;
+    protected Point2D min;
+    protected Point2D max;
+
     @Override
     public void mousePressed(MouseEvent event, Map map) {
         System.out.println("PenTool:MousePressed");
@@ -17,5 +22,31 @@ public abstract class PenTool implements DrawStrategy {
     @Override
     public void mouseReleased(MouseEvent event, Map map) {
         System.out.println("PenTool:MouseReleased");
+    }
+
+    protected void comparePoints(MouseEvent event, Map map) {
+        if(event.getX()-this.brushSize < min.getX()) {
+            min = new Point2D(event.getX()-this.brushSize, min.getY());
+        } else if(event.getX()+this.brushSize > max.getX()) {
+            max = new Point2D(event.getX()+this.brushSize, max.getY());
+        }
+
+        if(event.getY()-this.brushSize < min.getY()) {
+            min = new Point2D(min.getX(), event.getY()-this.brushSize);
+        } else if (event.getY()+this.brushSize > max.getY()) {
+            max = new Point2D(max.getX(), event.getY()+this.brushSize);
+        }
+
+        if(event.getX() < 0) {
+            min = new Point2D(0, min.getY());
+        } else if (event.getX() > map.getWidth()) {
+            max = new Point2D(map.getWidth(), max.getY());
+        }
+
+        if(event.getY() < 0) {
+            min = new Point2D(min.getX(), 0);
+        } else if(event.getY() > map.getHeight()) {
+            max = new Point2D(max.getX(), map.getHeight());
+        }
     }
 }
